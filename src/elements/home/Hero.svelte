f514a20f0d504ffa4a5550a1bd7ab6fcc2403b6c
WDangelo87 <noble8foldpath@proton.me>
2025-07-18 16:08:43 -0700

    Update Hero.svelte

--- src/elements/home/Hero.svelte
+++ src/elements/home/Hero.svelte
@@ -1,4 +1,5 @@
 <div class="flex gap-1 flex-col mb-12 items-center mt-40 md:text-[.5rem] md:mb-16 md:mt-48">
-    <h1 class="font-extrabold text-4xl md:text-6xl">Notcoin</h1>
-    <h2 class="text-base text-[#ebebf599] md:text-lg">the biggest web3 game</h2>
+  <img src="/images/tcg.webp" alt="Titan Cola Can" class="w-32 md:w-48" />
+  <h1 class="font-extrabold text-4xl md:text-6xl">Titan Cola God</h1>
+  <h2 class="text-base text-[#eb6f59] md:text-lg">the fizziest meme coin game</h2>
 </div>
