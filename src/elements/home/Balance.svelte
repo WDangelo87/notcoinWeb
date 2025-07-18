18f52f2288993c3af43a216c1310e8169ddc2a86
WDangelo87 <noble8foldpath@proton.me>
2025-07-18 16:44:37 -0700

    Update Balance.svelte

--- src/elements/home/Balance.svelte
+++ src/elements/home/Balance.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
   import { stat } from "../../store";
   import { animateValue } from "../../utils";
-  import Skeleton from "../../elements/misc/Skeleton.svelte";
+  import Skeleton from "../../misc/Skeleton.svelte";
 
   let objValue: any;
+
+  $: if (!stat.loading) {
+    stat.balanceCoins.then(value => {
+      objValue = value;
+    });
+  }
 </script>
 
 <div class="flex flex-col gap-2 items-center mb-6 md:mb-10">
-  <h3 class="text-[#ffffff99] uppercase text-xs md:text-base">total balance</h3>
+  <h3 class="text-[#ffffffb9] uppercase text-xs md:text-base">total balance</h3>
   <div class="flex gap-2 md:gap-4 items-center justify-center">
     <img
       class="w-10 h-10 md:w-[4.5rem] md:h-[4.5rem]"
@@ -20,11 +26,9 @@
       bind:this={objValue}
     >
       {#if stat.loading}
-        <Skeleton
-          className="w-44 h-7 sm:w-72 sm:h-10 md:w-96 md:h-14 md:rounded-lg lg:w-[32rem] lg:h-16 lg:rounded-xl"
-        />
-      {:else}
-        {@html animateValue(objValue, stat.balanceCoins)}
+        <Skeleton className="w-44 h-7 sm:w-72 sm:h-10 md:w-96 md:h-14 md:rounded-lg lg:w-[32rem] lg:h-16 lg:rounded-xl" />
+      {:else if objValue !== undefined}
+        {@html animateValue(objValue, objValue)}
       {/if}
     </div>
   </div>
