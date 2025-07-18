31cb60245a6d7782c5cca2af7ecdb733f01119ac
WDangelo87 <noble8foldpath@proton.me>
2025-07-18 16:48:19 -0700

    Update Skeleton.svelte

--- src/elements/misc/Skeleton.svelte
+++ src/elements/misc/Skeleton.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-    export let className: string, isSkeleton: boolean = true;
+  export let className: string;
+  export let isSkeleton: boolean = true;
 </script>
 
-<div class="{isSkeleton ? "skeleton top-0 bottom-0 left-0 right-0" : ""} {className}">
-    <div class="thickLine"></div>
+<div class="{isSkeleton ? 'skeleton top-0 bottom-0 left-0 right-0' : ''} {className}">
+  <div class="thickLine"></div>
 </div>
