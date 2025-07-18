85ce177eb7bd39a79b47f72b7fe3c109a0af51ed
WDangelo87 <noble8foldpath@proton.me>
2025-07-18 16:41:53 -0700

    Update Balance.svelte

--- src/elements/home/Balance.svelte
+++ src/elements/home/Balance.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-  import { $stat } from "../../store";
+  import { stat } from "../../store";
   import { animateValue } from "../../utils";
-
   import Skeleton from "../../elements/misc/Skeleton.svelte";
 
   let objValue: any;
@@ -20,12 +19,12 @@
       class="font-extrabold lining-nums tabular-nums text-2xl sm:text-3xl md:text-5xl lg:text-6xl"
       bind:this={objValue}
     >
-      {#if $stat.loading}
+      {#if stat.loading}
         <Skeleton
           className="w-44 h-7 sm:w-72 sm:h-10 md:w-96 md:h-14 md:rounded-lg lg:w-[32rem] lg:h-16 lg:rounded-xl"
         />
       {:else}
-        {@html animateValue(objValue, $stat.balanceCoins)}
+        {@html animateValue(objValue, stat.balanceCoins)}
       {/if}
     </div>
   </div>
