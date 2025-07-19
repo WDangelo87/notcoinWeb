ad16e9766a1ce9b9cac1ed0d434f6eedd63c727d
WDangelo87 <noble8foldpath@proton.me>
2025-07-18 17:09:20 -0700

    Update Balance.svelte

--- src/elements/home/Balance.svelte
+++ src/elements/home/Balance.svelte
@@ -4,19 +4,28 @@
 
 <div class="balance">
   <h1 class="title">
-    <img src="/coin.png" alt="Coin" width="48" height="48" />
+    <img src="/images/tcg_coin_512x512.png" alt="Coin" class="coin-icon" />
     <span>{totalBalance}</span>
   </h1>
 
   <ul class="stats">
     <li>
-      <span class="dot" /> 0 <span>total players</span>
+      <span class="dot" /> <span>total players</span>
     </li>
     <li>
-      <span class="dot" /> 0 <span>online today</span>
+      <span class="dot" /> <span>online today</span>
     </li>
     <li>
-      <span class="dot" /> 0 <span>online</span>
+      <span class="dot" /> <span>online</span>
     </li>
   </ul>
 </div>
+
+<style>
+  .coin-icon {
+    width: 32px;
+    height: 32px;
+    margin-right: 8px;
+    vertical-align: middle;
+  }
+</style>
